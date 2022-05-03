4fd9090dec7f4001ecd717e4c66889ac4679a4b2
Jhony Bryan López Somoza <Jhony.BLS@anacafe.org>
2022-05-03 14:51:42 -0600

    Se agrega los formularios de Iniciar sesion

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,11 +1,15 @@
 import { View, SafeAreaView, Text } from 'react-native'
 import React from 'react'
 
+import LoginForm from '../components/Auth/LoginForm';
+import UserData from '../components/Auth/UserData';
+
 const Account = () => {
+  const auth = null;
   return (
-    <SafeAreaView>
-      <Text>Account</Text>
-    </SafeAreaView>
+    <View>
+      {auth ? <UserData /> : <LoginForm />}
+    </View>
   )
 }
 
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+    username:"jblSomoza",
+    password:"12345"
+};
+
+export const userDetails = {
+    username:"jblSomoza",
+    firstName: "Jhony",
+    lastName: "Somoza",
+    email: "Jhony.BLS@anacafe.org"
+}
